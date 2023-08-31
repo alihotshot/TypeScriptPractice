d279cfe8d6c11c7660515291983cfdb6c7dff3b5
alihaidergithub <vergefuture@gmail.com>
2023-08-31 20:01:18 +0500

    31 aug

--- Assignments/calculator.ts
+++ Assignments/calculator.ts
@@ -1,1 +1,54 @@
-console.log('Hello');
+import inquirer from 'inquirer'
+
+async function main() {
+  const operationChoices = [
+    'Addition',
+    'Subtraction',
+    'Multiplication',
+    'Division',
+  ]
+
+  const operation = await inquirer.prompt({
+    type: 'list',
+    name: 'operation',
+    message: 'Select an operation:',
+    choices: operationChoices,
+  })
+
+  const num1 = parseFloat(
+    await inquirer.prompt({
+      type: 'input',
+      name: 'num1',
+      message: 'Enter the first number:',
+    })
+  )
+
+  const num2 = parseFloat(
+    await inquirer.prompt({
+      type: 'input',
+      name: 'num2',
+      message: 'Enter the second number:',
+    })
+  )
+
+  let result
+
+  switch (operation.operation) {
+    case 'Addition':
+      result = num1 + num2
+      break
+    case 'Subtraction':
+      result = num1 - num2
+      break
+    case 'Multiplication':
+      result = num1 * num2
+      break
+    case 'Division':
+      result = num1 / num2
+      break
+  }
+
+  console.log(`Result: ${result}`)
+}
+
+main()
