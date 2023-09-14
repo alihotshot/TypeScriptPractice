78199100a815020e4b156a6a0047bac649a76496
alihaidergithub <vergefuture@gmail.com>
2023-09-14 16:13:04 +0500

    14 Sep 2023
    14 Sep 2023

---
+++ TSPractice/Basics/basic.js
@@ -0,0 +1,13 @@
+/**** WHY TypeScript ****
+
+- Static Typing: TypeScript introduces static typing to JavaScript.
+This means that you can explicitly specify the types of variables, function parameters,
+and return values. This helps catch type-related errors at compile-time rather than runtime,
+making your code more robust and easier to maintain.
+
+- Improved Code Quality: With static typing, TypeScript can help you write cleaner
+and more self-documenting code.
+
+- Cross-Platform Development: TypeScript can be used for both front-end and back-end development. You can
+write TypeScript for browser-based applications (client-side) and Node.js applications (server-side) with ease.*/
+console.log('Hello');
--- TSPractice/Basics/basic.ts
+++ TSPractice/Basics/basic.ts
@@ -1,4 +1,4 @@
-/*                     **** WHY TypeScript ****
+/**** WHY TypeScript ****
 
 - Static Typing: TypeScript introduces static typing to JavaScript. 
 This means that you can explicitly specify the types of variables, function parameters, 
@@ -11,4 +11,4 @@ and more self-documenting code.
 - Cross-Platform Development: TypeScript can be used for both front-end and back-end development. You can 
 write TypeScript for browser-based applications (client-side) and Node.js applications (server-side) with ease.*/
 
-console.log()
+console.log('Hello')
