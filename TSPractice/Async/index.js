f3d8b5c9e0500f04b6ad9d1f83c6ed5a96d84a54
alihaidergithub <vergefuture@gmail.com>
2023-09-19 19:57:27 +0500

    19 Sep 2023
    19 Sep 2023

--- TSPractice/Async/index.js
+++ TSPractice/Async/index.js
@@ -1,4 +1,16 @@
-// Callbacks
+//Promises Notes Link: https://docs.google.com/document/d/19hpL-Dw4JgE-Dn1h_yc22rlJgJp_l42U8o4Wr0-0nKw/edit?usp=sharing
+/* Class Practice */
+//Normal Execution
+// Weekend Example
+/*
+- Car Maintenance
+- Laundary Dress
+- Attend Event
+*/
+console.log('I leave my car at 09:00 in Workshop...');
+console.log('Your Dress is Ready');
+console.log('Attend Event');
+/* Callbacks */
 function doSomething(Callbacks) {
     console.log('Doing Somethings ...');
     Callbacks();
